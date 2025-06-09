f8a78a8b6067c45d18e173fe1e8ce07ae868d594
AravindP07 <156054357+AravindP07@users.noreply.github.com>
2025-06-09 16:34:09 +0530

    Update client.c

--- networks/FTP over TCP/client.c
+++ networks/FTP over TCP/client.c
@@ -1,81 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-
-#define BUFFER_SIZE 1024
 
 int main() {
     int sock, n;
-    struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
-    FILE *file;
+    struct sockaddr_in server;
+    char buf[1024];
+    FILE *f;
 
-    // Create TCP socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        perror("Cannot create socket");
-        return 1;
-    }
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8888);
+    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
 
-    // Configure server address
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8888);  // Server port
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {  // Server IP (localhost)
-        perror("Invalid address/ Address not supported");
-        close(sock);
-        return 1;
-    }
+    connect(sock, (struct sockaddr*)&server, sizeof(server));
 
-    // Connect to the server
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connection failed");
-        close(sock);
-        return 1;
-    }
+    printf("File name: ");
+    scanf("%s", buf);
+    send(sock, buf, strlen(buf), 0);
 
-    printf("Connected to server.\n");
+    f = fopen(buf, "rb");
+    while ((n = fread(buf, 1, sizeof(buf), f)) > 0)
+        send(sock, buf, n, 0);
 
-    // Get file name from user
-    printf("Enter the file name to send: ");
-    scanf("%s", buffer);
-
-    // Send file name to server
-    if (send(sock, buffer, strlen(buffer), 0) < 0) {
-        perror("Send failed");
-        close(sock);
-        return 1;
-    }
-
-    // Open file for reading
-    file = fopen(buffer, "rb");
-    if (!file) {
-        perror("File open failed");
-        close(sock);
-        return 1;
-    }
-
-    // Send file data
-    while ((n = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
-        if (send(sock, buffer, n, 0) < 0) {
-            perror("Send failed");
-            fclose(file);
-            close(sock);
-            return 1;
-        }
-    }
-
-    // Properly signal end of file transmission
-    shutdown(sock, SHUT_WR);
-    printf("File sent successfully.\n");
-
-    // Close file and socket
-    fclose(file);
+    fclose(f);
     close(sock);
-
-    return 0;
 }
-
