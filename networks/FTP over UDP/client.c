ace845e2cc41fc85587e70124e15ff81ad8f8589
AravindP07 <156054357+AravindP07@users.noreply.github.com>
2025-06-09 16:39:54 +0530

    Update client.c

--- networks/FTP over UDP/client.c
+++ networks/FTP over UDP/client.c
@@ -1,56 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <fcntl.h>
 
 int main() {
-    int sockdes;
-    FILE *fd;
-    char filename[25], new_filename[25], filecontent[2000];
+    int sock, n;
     struct sockaddr_in server;
-    socklen_t c = sizeof(server);
+    socklen_t slen = sizeof(server);
+    char buf[1024];
+    FILE *f;
 
-    sockdes = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockdes == -1) {
-        printf("Cannot create socket\n");
-        return 1;
-    }
-
-    memset(&server, 0, sizeof(server));
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_port = htons(8888);
+    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
 
-    printf("Enter the file name: ");
-    scanf("%s", filename);
-    printf("Enter new file name: ");
-    scanf("%s", new_filename);
+    printf("File name: ");
+    scanf("%s", buf);
+    sendto(sock, buf, strlen(buf), 0, (void*)&server, slen);
 
-    sendto(sockdes, filename, strlen(filename), 0, (struct sockaddr*)&server, c);
+    f = fopen(buf, "rb");
+    while ((n = fread(buf, 1, sizeof(buf), f)) > 0)
+        sendto(sock, buf, n, 0, (void*)&server, slen);
 
-    int n = recvfrom(sockdes, filecontent, sizeof(filecontent) - 1, 0, (struct sockaddr*)&server, &c);
-    if (n < 0) {
-        printf("Error receiving file content\n");
-        return 1;
-    }
-    filecontent[n] = '\0';
+    buf[0] = '\0';
+    sendto(sock, buf, 1, 0, (void*)&server, slen);
 
-    if (strcmp(filecontent, "File not found") == 0) {
-        printf("Server: File not found\n");
-    } else {
-        fd = fopen(new_filename, "w");
-        if (fd == NULL) {
-            printf("Error creating file\n");
-            return 1;
-        }
-        fprintf(fd, "%s", filecontent);
-        fclose(fd);
-        printf("File received and saved as %s\n", new_filename);
-    }
-
-    close(sockdes);
-    return 0;
+    fclose(f);
+    close(sock);
 }
-
