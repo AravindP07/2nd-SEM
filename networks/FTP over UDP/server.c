12e6c629d401298300f9d42e591781a652e037b3
AravindP07 <156054357+AravindP07@users.noreply.github.com>
2025-06-09 16:42:21 +0530

    Update server.c

--- networks/FTP over UDP/server.c
+++ networks/FTP over UDP/server.c
@@ -1,52 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>	
-#include <sys/socket.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include <fcntl.h>
 
 int main() {
-    int sockdes, fd, n;
-    char fname[25] = "", fcontent[2000] = "";
+    int sock, n;
     struct sockaddr_in server, client;
-    socklen_t c = sizeof(client);
+    socklen_t clen = sizeof(client);
+    char buf[1024];
+    FILE *f;
 
-    sockdes = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockdes == -1) {
-        printf("Cannot create socket\n");
-        return 1;
-    }
-
-    memset(&server, 0, sizeof(server));
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(8888);
+    server.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(sockdes, (struct sockaddr*)&server, sizeof(server)) < 0) {
-        printf("Bind failed\n");
-        return 1;
+    bind(sock, (void*)&server, sizeof(server));
+
+    n = recvfrom(sock, buf, sizeof(buf), 0, (void*)&client, &clen);
+    buf[n] = '\0';
+    f = fopen(buf, "wb");
+
+    while (1) {
+        n = recvfrom(sock, buf, sizeof(buf), 0, (void*)&client, &clen);
+        if (n == 1 && buf[0] == '\0') break;
+        fwrite(buf, 1, n, f);
     }
 
-    printf("Bind done\n");
-
-    n = recvfrom(sockdes, fname, sizeof(fname) - 1, 0, (struct sockaddr*)&client, &c);
-    if (n < 0) {
-        printf("Error receiving filename\n");
-        return 1;
-    }
-    fname[n] = '\0';
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        sendto(sockdes, "File not found", 15, 0, (struct sockaddr*)&client, c);
-    } else {
-        n = read(fd, fcontent, sizeof(fcontent) - 1);
-        fcontent[n] = '\0';
-        sendto(sockdes, fcontent, n, 0, (struct sockaddr*)&client, c);
-        close(fd);
-    }
-
-    close(sockdes);
-    return 0;
+    fclose(f);
+    close(sock);
 }
 
